99298a912b2458719ca5ce43c41f9829aad9f378
abdullah-al-mamun-udoy <udoygithub@gmail.com>
2025-05-13 20:12:40 +0600

    created quiz can be fetched from db, and created a wrapper class for only quiz question

--- src/main/java/com/udoy/quizapp/service/QuizService.java
+++ src/main/java/com/udoy/quizapp/service/QuizService.java
@@ -1,6 +1,7 @@
 package com.udoy.quizapp.service;
 
 import com.udoy.quizapp.model.Question;
+import com.udoy.quizapp.model.QuestionWrapper;
 import com.udoy.quizapp.model.Quiz;
 import com.udoy.quizapp.dao.QuestionDao;
 import com.udoy.quizapp.dao.QuizDao;
@@ -9,7 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -31,5 +34,17 @@ public class QuizService {
 
         return new ResponseEntity<>("success save", HttpStatus.OK);
 
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionForUser = new ArrayList<>();
+        for (Question q: questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+            questionForUser.add(qw);
+        }
+        return new ResponseEntity<>(questionForUser, HttpStatus.OK);
     }
 }
