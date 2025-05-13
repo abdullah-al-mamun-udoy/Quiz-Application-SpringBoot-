a2b15637c02b92270084c4a8b9568c77cf10fa9f
abdullah-al-mamun-udoy <udoygithub@gmail.com>
2025-05-13 15:06:25 +0600

    exception handled with status code

--- src/main/java/com/udoy/quizapp/controller/QuestionController.java
+++ src/main/java/com/udoy/quizapp/controller/QuestionController.java
@@ -3,10 +3,8 @@ package com.udoy.quizapp.controller;
 import com.udoy.quizapp.Question;
 import com.udoy.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +16,19 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{categ}")
-    public List<Question> getAllQuestionsByCategory(@PathVariable("categ") String category){
+    public ResponseEntity<List<Question>> getAllQuestionsByCategory(@PathVariable("categ") String category){
         return questionService.getAllQuestionsByCategory(category);
     }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+
+
 }
--- src/main/java/com/udoy/quizapp/service/QuestionService.java
+++ src/main/java/com/udoy/quizapp/service/QuestionService.java
@@ -4,8 +4,12 @@ package com.udoy.quizapp.service;
 import com.udoy.quizapp.Question;
 import com.udoy.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,12 +19,31 @@ public class QuestionService {
     QuestionDao questionDao;
 
 
-    public List<Question> getAllQuestions() {
-       return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
 
 
-    public List<Question> getAllQuestionsByCategory(String category) {
-        return  questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getAllQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>( questionDao.findByCategory(category), HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 }
